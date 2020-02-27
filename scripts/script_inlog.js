8019cd242e9250de3758d01f8dee865f5a4ed859
Zegert Boele <48018787+ZegertBoele@users.noreply.github.com>
2020-02-27 17:55:37 +0100

    css inplant

---
+++ scripts/script_inlog.js
@@ -0,0 +1,25 @@
+console.log("Yeet");
+// Get the modal
+var modal1 = document.getElementById('id01');
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+}
+// Get the modal
+var modal2 = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+}
+function toggle() {
+    var element = document.getElementById("toggle1");
+    element.classList.toggle("onzichtbaar");
+    var element = document.getElementById("toggle2");
+    element.classList.toggle("onzichtbaar");
+
+}
