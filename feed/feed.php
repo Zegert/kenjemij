794d42d903567fae21d38eca347b597e66c8b7c1
Zegert Boele <48018787+ZegertBoele@users.noreply.github.com>
2020-03-02 15:40:21 +0100

    feed fix 1.2

--- feed/feed.php
+++ feed/feed.php
@@ -11,6 +11,7 @@ require_once('../Includes/config.php');
     <title>Ken jij mij</title>
     <!-- Stylesheets hier -->
 </head>
+<<<<<<< Updated upstream
 <body>
     <a href="feed_nieuw.php"><div><p>Voeg zelf een feit/leugen toe</p></div></a>
     <?php 
@@ -56,6 +57,38 @@ require_once('../Includes/config.php');
                 echo "<br>";
         echo "</div>";
             }
+=======
+
+<body>
+    <a href="feed_nieuw.php">
+        <div>
+            <p>Voeg zelf een feit/leugen toe</p>
+        </div>
+    </a>
+    <?php
+    $sql = "SELECT * FROM feed";
+    $feedArray = mysqli_query($mysqli, $sql);
+    echo "<div id='feed_container'>";
+    while ($row = $feedArray->fetch_assoc()) {
+
+        $itemID = $row['ID'];
+        $id = $row['userID'];
+        $sqlUN = "SELECT `userName` FROM `users` WHERE ID=" . $id;
+        $UNArray = mysqli_query($mysqli, $sqlUN);
+        while ($rowUN = $UNArray->fetch_assoc()) {
+            $username = $rowUN['userName'];
+        }
+        echo "<p>Gemaakt door: " . $username . "</p>";
+        echo '<p>Titel: ' . $row['titel'];
+        '</p>';
+        echo '<p>Beschrijving: ' . $row['beschrijving'];
+        '</p>';
+        echo "<br><a href='waar.php?itemID=$itemID'>Waar(" . $row['waar'] . ")</a> of <a href='nietwaar.php?itemID=$itemID'>Niet waar(" . $row['nietwaar'] . ")</a><br>";
+        echo "<br>";
+    }
+    echo "</div>";
+>>>>>>> Stashed changes
     ?>
 </body>
+
 </html>
