11630d97083bb9305084eded019f28744f4e99eb
Zegert Boele <48018787+ZegertBoele@users.noreply.github.com>
2020-03-03 11:44:05 +0100

    Update feed.php

--- feed/feed.php
+++ feed/feed.php
@@ -10,23 +10,11 @@ require_once('../Includes/config.php');
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ken jij mij</title>
     <!-- Stylesheets hier -->
+    <link rel="stylesheet" type="text/css" href="../CSS/style.css">
 </head>
-<<<<<<< Updated upstream
 <body>
-    <a href="feed_nieuw.php"><div><p>Voeg zelf een feit/leugen toe</p></div></a>
-    <?php 
-        $sql = "SELECT * FROM feed";
-        $feedArray = mysqli_query($mysqli , $sql);
-        echo "<div id='feed_container'>";
-            while($row = $feedArray->fetch_assoc()){
 
-                $itemID = $row['ID'];
-                $id = $row['userID'];
-                $sqlUN = "SELECT `userName` FROM `users` WHERE ID=" . $id;
-                $UNArray = mysqli_query($mysqli , $sqlUN);
-                while($rowUN = $UNArray->fetch_assoc()){
-                    $username = $rowUN['userName'];
-?>
+<!--BEGIN NAVBAR-->
 <div class="topnav">
     <a href="../profiel.php">Profiel</a>
     <a href="../uitlog.php" style="float:right">Uitloggen</a>
@@ -50,45 +38,43 @@ require_once('../Includes/config.php');
             while ($row = mysqli_fetch_array($result))
             {
                 echo "<td>" . $row['userName'] . "</td>";
-                echo "<p>Gemaakt door: " . $username . "</p>";
-                echo '<p>Titel: ' . $row['titel']; '</p>';
-                echo '<p>Beschrijving: ' . $row['beschrijving']; '</p>';
-                echo "<br><a href='waar.php?itemID=$itemID'>Waar(" . $row['waar'] .")</a> of <a href='nietwaar.php?itemID=$itemID'>Niet waar(". $row['nietwaar'] .")</a><br>";
-                echo "<br>";
-        echo "</div>";
             }
-=======
 
-<body>
-    <a href="feed_nieuw.php">
-        <div>
-            <p>Voeg zelf een feit/leugen toe</p>
+            if ($result == $row)
+            {
+                {
+                    ?>
+
+                    <?php
+                }
+            }
+            ?>
         </div>
-    </a>
+    </div>
     <?php
     $sql = "SELECT * FROM feed";
-    $feedArray = mysqli_query($mysqli, $sql);
+    $feedArray = mysqli_query($mysqli , $sql);
     echo "<div id='feed_container'>";
-    while ($row = $feedArray->fetch_assoc()) {
+    while($row = $feedArray->fetch_assoc()){
 
         $itemID = $row['ID'];
         $id = $row['userID'];
         $sqlUN = "SELECT `userName` FROM `users` WHERE ID=" . $id;
-        $UNArray = mysqli_query($mysqli, $sqlUN);
-        while ($rowUN = $UNArray->fetch_assoc()) {
+        $UNArray = mysqli_query($mysqli , $sqlUN);
+        while($rowUN = $UNArray->fetch_assoc()){
             $username = $rowUN['userName'];
         }
+
+        echo "<div class='container'>";
         echo "<p>Gemaakt door: " . $username . "</p>";
-        echo '<p>Titel: ' . $row['titel'];
-        '</p>';
-        echo '<p>Beschrijving: ' . $row['beschrijving'];
-        '</p>';
-        echo "<br><a href='waar.php?itemID=$itemID'>Waar(" . $row['waar'] . ")</a> of <a href='nietwaar.php?itemID=$itemID'>Niet waar(" . $row['nietwaar'] . ")</a><br>";
+        echo '<h3 class="kop">Titel: ' . $row['titel']; '</p>';
+        echo '<p class="stelling_text">Beschrijving: ' . $row['beschrijving']; '</p>';
         echo "<br>";
+        echo "<br><a class=\"goed\" href='waar.php?itemID=$itemID'>Waar(" . $row['waar'] .")</a><a class='fout' href='nietwaar.php?itemID=$itemID'>Niet waar(". $row['nietwaar'] .")</a><br>";
+        echo "<br>";
+        echo "</div>";
     }
     echo "</div>";
->>>>>>> Stashed changes
     ?>
 </body>
-
 </html>
